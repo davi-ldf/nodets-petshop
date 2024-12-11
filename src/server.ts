34ca8cc3c83dac5b16ec640dba7add206536d2e8
davi-ldf <davifranca135@gmail.com>
2024-12-11 17:03:43 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,9 +8,10 @@ dotenv.config();
 
 const server = express();
 
+server.engine('mustache', mustache());
 server.set('view engine', 'mustache');
 server.set('views', path.join(__dirname, 'views'));
-server.set('mustache', mustache());
+
 
 server.use(express.static(path.join(__dirname, '../public')));
 
