dd458eb31f11d16c5d6d1f711deea281c4a15df4
davi-ldf <davifranca135@gmail.com>
2024-12-11 16:04:23 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRoutes from './routes/index';
 
 dotenv.config();
 
@@ -14,7 +15,10 @@ server.set('mustache', mustache());
 server.use(express.static(path.join(__dirname, '../public')));
 
 // Rotas
+server.use(mainRoutes);
 
-server.listen(process.env.PORT, () => {
-    console.log("Servidor rodando em http://localhost:4000/");
+server.use((req, res) => {
+    res.send('Página não encontrada');
 });
+
+server.listen(process.env.PORT);
