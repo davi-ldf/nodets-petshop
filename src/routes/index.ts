3523e71c294d5ff8ce2674333ac1d74bafa36199
davi-ldf <davifranca135@gmail.com>
2024-12-11 16:11:05 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import * as PageController from '../controllers/pageController';
+import * as SearchController from '../controllers/searchController';
 
 const router = Router();
 
