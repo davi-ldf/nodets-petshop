41a5b8830080f44211e323adbce382084f47ba2e
davi-ldf <davifranca135@gmail.com>
2024-12-13 22:15:56 -0300

    Update searchController.ts
    Implementação da busca

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObject';
+
 export const search = (req: Request, res: Response) => {
-    
-    //res.render('pages/search');
+    let query = req.query.q as string;
+
+    let list = Pet.getFromName(query);
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list
+    });
 }
